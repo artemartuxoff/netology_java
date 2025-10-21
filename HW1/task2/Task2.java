f76fd1b22eadfa022373237b5fc4f8cbec8e9a83
Artem <artem.artuxoff@yandex.ru>
2025-10-21 08:52:07 +0300

    first upload

---
+++ HW1/task2/Task2.java
@@ -0,0 +1,9 @@
+public class Task2 {
+    public static void main(String[] args) {
+        int income = 44000;
+        int spending = 30000;
+
+        System.out.println("Итого (руб):");
+        System.out.println(income-spending);
+    }
+}
