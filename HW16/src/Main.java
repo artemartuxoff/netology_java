84fa1a7bee00a4ebc96f259b13a600ce03c49f7e
admin <artem.artuxoff@yandex.ru>
2025-12-20 11:11:37 +0300

    TreeSet

---
+++ HW16/src/Main.java
@@ -0,0 +1,104 @@
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.TreeSet;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        TreeSet<Person> candidates = new TreeSet<>(new SpacePersonComparator());
+        candidates.add(new Person("Sonya Popova", 35, 15));
+        candidates.add(new Person("Dazdraperma Sponzhova", 33, 15));
+        candidates.add(new Person("Salavat Netologshvili", 23, 13));
+        candidates.add(new Person("Sasha Sun", 31, 15));
+        candidates.add(new Person("Svetlana Morkov", 38, 15));
+        candidates.add(new Person("Sasha Sosnova", 38, 11));
+
+        Iterator<Person> it = candidates.iterator();
+        System.out.println(it.next());
+        System.out.println(it.next());
+    }
+}
+
+
+class Person {
+    private String name;
+    private int age;
+    private int experience;
+
+    public Person(String name, int age, int experience) {
+        this.name = name;
+        this.age = age;
+        this.experience = experience;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public int getExperience() {
+        return experience;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
+
+
+class SpacePersonComparator implements Comparator<Person> {
+
+    @Override
+    public int compare(Person p1, Person p2){
+
+        if(p1.getExperience()>p2.getExperience()){
+            return -1;}
+        else if(p1.getExperience()<p2.getExperience()){
+            return 1;}
+        else{
+            String name1 = p1.getName();
+            String name2 = p2.getName();
+
+            int countName1 = countChar(name1);
+            int countName2 = countChar(name2);
+
+            if(countName1>countName2){
+                return -1;}
+            else if(countName1<countName2){
+                return 1;}
+            else{
+                if(name1.length()>name2.length()){
+                    return 1;
+                }
+                else if(name1.length()<name2.length()){
+                    return -1;
+                }
+                else{
+                    return 0;
+                }
+
+            }
+        }
+    }
+
+    public int countChar(String name){
+
+        name = name.toLowerCase();
+
+        int count = 0;
+
+        for (int i = 0; i < name.length(); i++) {
+            char a = name.charAt(i);
+
+            if(a == 's'){
+                count++;};
+        }
+        return count;
+
+    }
+}
